33f9dc5b302df2e0cc17240b5c73548fb039c81b
Codi Kraus <codi.kraus@gmail.com>
2021-04-29 01:21:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    message = "The line is currently: "
+    message = "The line is currently:"
     katz_deli.each_with_index do |position, index|
-      message << "#{index.to_i + 1}. #{position} "
+      message << " #{index.to_i + 1}. #{position}"
     end
     puts message
   end
