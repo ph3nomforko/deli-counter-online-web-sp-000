789f86718b4e4dbd696eedca03d609b8fdecde72
Codi Kraus <codi.kraus@gmail.com>
2021-04-29 00:43:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,11 @@
 # Write your code here.
 katz_deli = []
 
-def line(katz_deli)
+def line
   if katz_deli == nil
     "The line is currently empty."
   end
+end
+
+def take_a_number(katz_deli, name)
 end
