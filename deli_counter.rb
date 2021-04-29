41967dbf3500adeb55575a6afdaf70a2085518f7
Codi Kraus <codi.kraus@gmail.com>
2021-04-29 00:51:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,10 +5,10 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    "The line is currently: "
+    puts "The line is currently: "
     line_position = 1
     katz_deli.each do |position|
-      "#{line_position}. #{position} "
+      puts "#{line_position}. #{position} "
       line_position += 1
     end
   end
