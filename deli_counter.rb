8298918c48f9fcc3d330cb0e86145e287a58366b
Codi Kraus <codi.kraus@gmail.com>
2021-04-29 01:02:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,13 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Current serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
+end
+    
